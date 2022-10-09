a923e558b090a610222ed894534cd8808652a8da
yuk6ra <yuki.imamura.all@gmail.com>
2022-10-09 16:19:29 +0900

    done

--- contracts/Web3Mint.sol
+++ contracts/Web3Mint.sol
@@ -3,30 +3,62 @@
 
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "./libraries/Base64.sol";
 import "hardhat/console.sol";
 
-contract Web3Mint is ERC721URIStorage {
-    using Counters for Counters.Counter;
+contract Web3Mint is ERC721 {
 
-    Counters.Counter private _tokenIds;
-
-    constructor() ERC721("myNFT","MYN") {
-        console.log("This is my NFT contract.");
+    struct NftAttributes{
+        string name;
+        string imageURL;
     }
 
-    function makeAnEpicNFT() public{
-        
+    NftAttributes[] Web3Nfts;
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("myNFT", "MYN") {
+        console.log("This is my NFT contract");
+    }
+
+    function mintIpfsNFT(string memory name, string memory imageURL) public {
         uint256 newItemId = _tokenIds.current();
 
         _safeMint(msg.sender, newItemId);
 
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/5PYB");
+        Web3Nfts.push(NftAttributes({
+            name: name,
+            imageURL: imageURL
+        }));
 
         console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
-
         _tokenIds.increment();
     }
-    
+
+    function tokenURI(uint256 _tokenId) public override view returns(string memory) {
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        Web3Nfts[_tokenId].name,
+                        '---NFT #',
+                        Strings.toString(_tokenId),
+                        '","description": "An epic NFT", "image": "ipfs://', 
+                        Web3Nfts[_tokenId].imageURL,'"}'
+                    )
+                )
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+        return output;
+    }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,26 +1,25 @@
 // run.js
-
 const main = async () => {
     const nftContractFactory = await hre.ethers.getContractFactory("Web3Mint");
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
-    console.log("Contract deployed to: ", nftContract.address);
+    console.log("Contract deployed to:", nftContract.address);
 
-    let txn = await nftContract.makeAnEpicNFT();
+    let txn = await nftContract.mintIpfsNFT("poker","bafybeibewfzz7w7lhm33k2rmdrk3vdvi5hfrp6ol5vhklzzepfoac37lry");
+
     await txn.wait();
-
-    txn = await nftContract.makeAnEpicNFT();
-    await txn.wait();
-}
-
-const runMain = async () => {
-    try{
-        await main();
-        process.exit(0);
+    let returnedTokenUri = await nftContract.tokenURI(0);
+    console.log("tokenURI:",returnedTokenUri);
+  };
+  // エラー処理を行っています。
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
     } catch (error) {
-        console.log(error);
-        process.exit(1);
+      console.log(error);
+      process.exit(1);
     }
-};
+  };
 
-runMain();
+  runMain();
